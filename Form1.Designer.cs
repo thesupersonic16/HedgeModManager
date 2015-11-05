f4f3eb5fa08e73702ab8f3ca76859b880a427348
Radfordhound <Radfordhound@gmail.com>
2015-11-05 05:34:13 -0600

    Added initial source
    Currently a WIP

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace SLWModLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.modslist = new System.Windows.Forms.ListView();
+            this.nameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.vcolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.authorcolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.savecolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.updatedcolumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.MoveDownAll = new System.Windows.Forms.Button();
+            this.MoveUpAll = new System.Windows.Forms.Button();
+            this.MoveDownbtn = new System.Windows.Forms.Button();
+            this.MoveUpbtn = new System.Windows.Forms.Button();
+            this.modsdirbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.modsdir = new System.Windows.Forms.TextBox();
+            this.nomodsfound = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.playbtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // modslist
+            // 
+            this.modslist.CheckBoxes = true;
+            this.modslist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameColumn,
+            this.vcolumn,
+            this.authorcolumn,
+            this.savecolumn,
+            this.updatedcolumn});
+            this.modslist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.modslist.Location = new System.Drawing.Point(0, 0);
+            this.modslist.MultiSelect = false;
+            this.modslist.Name = "modslist";
+            this.modslist.Size = new System.Drawing.Size(515, 409);
+            this.modslist.TabIndex = 0;
+            this.modslist.UseCompatibleStateImageBehavior = false;
+            this.modslist.View = System.Windows.Forms.View.Details;
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.Text = "Name";
+            this.nameColumn.Width = 175;
+            // 
+            // vcolumn
+            // 
+            this.vcolumn.Text = "V";
+            this.vcolumn.Width = 31;
+            // 
+            // authorcolumn
+            // 
+            this.authorcolumn.Text = "Author";
+            this.authorcolumn.Width = 119;
+            // 
+            // savecolumn
+            // 
+            this.savecolumn.Text = "Save";
+            this.savecolumn.Width = 66;
+            // 
+            // updatedcolumn
+            // 
+            this.updatedcolumn.Text = "Updated";
+            this.updatedcolumn.Width = 90;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.modsdirbtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.modsdir);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 50, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(548, 459);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 50);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.modslist);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.MoveDownAll);
+            this.splitContainer1.Panel2.Controls.Add(this.MoveUpAll);
+            this.splitContainer1.Panel2.Controls.Add(this.MoveDownbtn);
+            this.splitContainer1.Panel2.Controls.Add(this.MoveUpbtn);
+            this.splitContainer1.Size = new System.Drawing.Size(548, 409);
+            this.splitContainer1.SplitterDistance = 515;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // MoveDownAll
+            // 
+            this.MoveDownAll.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MoveDownAll.Location = new System.Drawing.Point(3, 306);
+            this.MoveDownAll.Name = "MoveDownAll";
+            this.MoveDownAll.Size = new System.Drawing.Size(19, 100);
+            this.MoveDownAll.TabIndex = 3;
+            this.MoveDownAll.Text = "↓";
+            this.MoveDownAll.UseVisualStyleBackColor = true;
+            this.MoveDownAll.Click += new System.EventHandler(this.MoveDownAll_Click);
+            // 
+            // MoveUpAll
+            // 
+            this.MoveUpAll.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MoveUpAll.Location = new System.Drawing.Point(3, 3);
+            this.MoveUpAll.Name = "MoveUpAll";
+            this.MoveUpAll.Size = new System.Drawing.Size(19, 100);
+            this.MoveUpAll.TabIndex = 2;
+            this.MoveUpAll.Text = "↑";
+            this.MoveUpAll.UseVisualStyleBackColor = true;
+            this.MoveUpAll.Click += new System.EventHandler(this.MoveUpAll_Click);
+            // 
+            // MoveDownbtn
+            // 
+            this.MoveDownbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MoveDownbtn.Location = new System.Drawing.Point(3, 205);
+            this.MoveDownbtn.Name = "MoveDownbtn";
+            this.MoveDownbtn.Size = new System.Drawing.Size(19, 100);
+            this.MoveDownbtn.TabIndex = 1;
+            this.MoveDownbtn.Text = "▼";
+            this.MoveDownbtn.UseVisualStyleBackColor = true;
+            this.MoveDownbtn.Click += new System.EventHandler(this.MoveDownbtn_Click);
+            // 
+            // MoveUpbtn
+            // 
+            this.MoveUpbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MoveUpbtn.Location = new System.Drawing.Point(3, 104);
+            this.MoveUpbtn.Name = "MoveUpbtn";
+            this.MoveUpbtn.Size = new System.Drawing.Size(19, 100);
+            this.MoveUpbtn.TabIndex = 0;
+            this.MoveUpbtn.Text = "▲";
+            this.MoveUpbtn.UseVisualStyleBackColor = true;
+            this.MoveUpbtn.Click += new System.EventHandler(this.MoveUpbtn_Click);
+            // 
+            // modsdirbtn
+            // 
+            this.modsdirbtn.AutoSize = true;
+            this.modsdirbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.modsdirbtn.Location = new System.Drawing.Point(467, 6);
+            this.modsdirbtn.Name = "modsdirbtn";
+            this.modsdirbtn.Size = new System.Drawing.Size(49, 29);
+            this.modsdirbtn.TabIndex = 3;
+            this.modsdirbtn.Text = "...";
+            this.modsdirbtn.UseVisualStyleBackColor = true;
+            this.modsdirbtn.Click += new System.EventHandler(this.modsdirbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SLW directory:";
+            // 
+            // modsdir
+            // 
+            this.modsdir.Location = new System.Drawing.Point(131, 9);
+            this.modsdir.Name = "modsdir";
+            this.modsdir.Size = new System.Drawing.Size(330, 26);
+            this.modsdir.TabIndex = 1;
+            this.modsdir.TextChanged += new System.EventHandler(this.modsdir_TextChanged);
+            // 
+            // nomodsfound
+            // 
+            this.nomodsfound.AutoSize = true;
+            this.nomodsfound.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.nomodsfound.ForeColor = System.Drawing.Color.Red;
+            this.nomodsfound.Location = new System.Drawing.Point(138, 201);
+            this.nomodsfound.Name = "nomodsfound";
+            this.nomodsfound.Size = new System.Drawing.Size(272, 56);
+            this.nomodsfound.TabIndex = 4;
+            this.nomodsfound.Text = "No mods found!\r\nPlease check your mod folder.";
+            this.nomodsfound.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.nomodsfound.Visible = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.linkLabel1.Location = new System.Drawing.Point(211, 271);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(127, 21);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Refresh Mod List";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // playbtn
+            // 
+            this.playbtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.playbtn.Location = new System.Drawing.Point(17, 476);
+            this.playbtn.Name = "playbtn";
+            this.playbtn.Size = new System.Drawing.Size(515, 43);
+            this.playbtn.TabIndex = 3;
+            this.playbtn.Text = "Play";
+            this.playbtn.UseVisualStyleBackColor = true;
+            this.playbtn.Click += new System.EventHandler(this.playbtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(124, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh Mod List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(548, 563);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nomodsfound);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.playbtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SLW Mod Loader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView modslist;
+        private System.Windows.Forms.ColumnHeader nameColumn;
+        private System.Windows.Forms.ColumnHeader vcolumn;
+        private System.Windows.Forms.ColumnHeader authorcolumn;
+        private System.Windows.Forms.ColumnHeader savecolumn;
+        private System.Windows.Forms.ColumnHeader updatedcolumn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox modsdir;
+        private System.Windows.Forms.Button modsdirbtn;
+        private System.Windows.Forms.Button playbtn;
+        private System.Windows.Forms.Label nomodsfound;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button MoveDownbtn;
+        private System.Windows.Forms.Button MoveUpbtn;
+        private System.Windows.Forms.Button MoveDownAll;
+        private System.Windows.Forms.Button MoveUpAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SLWModLoader
+{
+    public partial class Form1 : Form
+    {
+        public static string versionstring = "1.4";
+        public static Thread cpkpackthread, loadmodthread;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            modsdir.Text = @"C:\Program Files (x86)\Steam\SteamApps\common\Sonic Lost World";
+            if (File.Exists(Application.StartupPath + "\\config.txt")) { modsdir.Text = File.ReadAllLines(Application.StartupPath + "\\config.txt")[1]; }
+
+            if (Directory.Exists(modsdir.Text))
+            {
+                if (!Directory.Exists(modsdir.Text+"\\mods") && MessageBox.Show("A \"mods\" folder does not exist within your Sonic Lost World installation directory. Would you like to create one?", "SLW Mod Loader", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) Directory.CreateDirectory(modsdir.Text + "\\mods");
+            }
+            else { MessageBox.Show("SLW Mod Loader could not find your Sonic Lost World installation directory. You'll have to manually set your mod directory.","SLW Mod Loader",MessageBoxButtons.OK,MessageBoxIcon.Warning); modsdir.Text = ""; }
+
+            Text = $"SLW Mod Loader (v {versionstring})";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cpkpackthread = new Thread(new ThreadStart(PackCPK));
+            loadmodthread = new Thread(new ThreadStart(LoadMods));
+            loadmodthread.Start();
+        }
+
+        private void LoadMods()
+        {
+            Invoke(new Action(() => modslist.Items.Clear()));
+            if (!string.IsNullOrEmpty(modsdir.Text) && Directory.Exists(modsdir.Text+"\\mods"))
+            {
+                foreach (string mod in Directory.GetDirectories(modsdir.Text+"\\mods"))
+                {
+                    Invoke(new Action(() => { modslist.Items.Add(new DirectoryInfo(mod).Name); }));
+                }
+            }
+
+            Invoke(new Action(() => { nomodsfound.Visible = linkLabel1.Visible = (modslist.Items.Count < 1); modslist.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize); })); 
+        }
+
+        private void PackCPK()
+        {
+            //Make a backup for going back to vanilla Lost World if one doesn't already exist
+            if (!File.Exists(modsdir.Text + "\\disk\\sonic2013_patch_0_Backup.cpk") && File.Exists(modsdir.Text + "\\disk\\sonic2013_patch_0.cpk")) { File.Copy(modsdir.Text + "\\disk\\sonic2013_patch_0.cpk", modsdir.Text + "\\disk\\sonic2013_patch_0_Backup.cpk"); }
+
+            //Re-pack the cpk using cpkmackec.exe
+            using (Process proc = new Process())
+            {
+                proc.StartInfo = new ProcessStartInfo("cpkmakec.exe", $"\"{Application.StartupPath}\\temp\" \"{modsdir.Text}\\disk\\sonic2013_patch_0.cpk\" -align=2048 -code=UTF-8 -mode=FILENAME -mask");
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.Start();
+                proc.WaitForExit();
+            }
+
+            //Delete the temp folder
+            Directory.Delete(Application.StartupPath+"\\temp", true);
+
+            //Start Sonic Lost World
+            Process slw = new Process();
+            slw.StartInfo = new ProcessStartInfo(Application.StartupPath+"\\Sonic Lost World.url");
+
+            Invoke(new Action(() => { Close(); }));
+            slw.Start();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            loadmodthread = new Thread(new ThreadStart(LoadMods));
+            loadmodthread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadmodthread = new Thread(new ThreadStart(LoadMods));
+            loadmodthread.Start();
+        }
+
+        //Up...
+        private void MoveUpbtn_Click(object sender, EventArgs e)
+        {
+            if (modslist.SelectedItems.Count > 0 && modslist.SelectedItems[0].Index > 0)
+            {
+                int index = modslist.SelectedItems[0].Index - 1;
+                ListViewItem lvi = modslist.SelectedItems[0];
+                modslist.Items.RemoveAt(modslist.SelectedItems[0].Index);
+                modslist.Items.Insert(index, lvi);
+            }
+        }
+
+        //..and down and all around.
+        private void MoveDownbtn_Click(object sender, EventArgs e)
+        {
+            if (modslist.SelectedItems.Count > 0 && modslist.SelectedItems[0].Index < modslist.Items.Count - 1)
+            {
+                int index = modslist.SelectedItems[0].Index + 1;
+                ListViewItem lvi = modslist.SelectedItems[0];
+                modslist.Items.RemoveAt(modslist.SelectedItems[0].Index);
+                modslist.Items.Insert(index, lvi);
+            }
+        }
+
+        #region SHHHH... DON'T LOOK! IT'S A SECRET!!!
+        private void modsdir_TextChanged(object sender, EventArgs e)
+        {
+            if (modsdir.Text == "DO A BARREL ROLL") { label1.Font = nomodsfound.Font = linkLabel1.Font = playbtn.Font = button1.Font = modsdirbtn.Font = modslist.Font = new Font("Comic Sans MS",8);  }
+        }
+        #endregion
+
+        private void playbtn_Click(object sender, EventArgs e)
+        {
+            if (modslist.CheckedItems.Count > 0)
+            {
+                //Make a temporary directory
+                if (Directory.Exists(Application.StartupPath + "\\temp")) { Directory.Delete(Application.StartupPath + "\\temp", true); }
+                Directory.CreateDirectory(Application.StartupPath + "\\temp");
+
+                //Copy all the stuff there
+                foreach (ListViewItem mod in modslist.CheckedItems)
+                {
+                    if (Directory.Exists(modsdir.Text + "\\mods\\" + mod.Text))
+                    {
+                        DirectoryCopy(modsdir.Text + "\\mods\\" + mod.Text, Application.StartupPath + "\\temp", true);
+                    }
+                }
+
+                cpkpackthread.Start();
+            }
+            else
+            {
+                if (File.Exists(modsdir.Text + "\\disk\\sonic2013_patch_0_Backup.cpk")) { File.Copy(modsdir.Text + "\\disk\\sonic2013_patch_0_Backup.cpk", modsdir.Text + "\\disk\\sonic2013_patch_0.cpk", true); }
+                
+                //Start Sonic Lost World
+                Process slw = new Process();
+                slw.StartInfo = new ProcessStartInfo(Application.StartupPath + "\\Sonic Lost World.url");
+
+                slw.Start();
+                Close();
+            }
+        }
+
+        private void modsdirbtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog() { ShowNewFolderButton = true, Description = "The folder which contains the Sonic Lost World mods you wish to load." };
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                modsdir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void MoveUpAll_Click(object sender, EventArgs e)
+        {
+            if (modslist.SelectedItems.Count > 0 && modslist.SelectedItems[0].Index > 0)
+            {
+                ListViewItem lvi = modslist.SelectedItems[0];
+                modslist.Items.RemoveAt(modslist.SelectedItems[0].Index);
+                modslist.Items.Insert(0, lvi);
+            }
+        }
+
+        private void MoveDownAll_Click(object sender, EventArgs e)
+        {
+            if (modslist.SelectedItems.Count > 0 && modslist.SelectedItems[0].Index < modslist.Items.Count)
+            {
+                ListViewItem lvi = modslist.SelectedItems[0];
+                modslist.Items.RemoveAt(modslist.SelectedItems[0].Index);
+                modslist.Items.Insert(0, lvi);
+            }
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "\\config.txt")) { File.Delete(Application.StartupPath + "\\config.txt"); }
+            File.WriteAllLines(Application.StartupPath + "\\config.txt",new string[] { versionstring, modsdir.Text });
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                if (!File.Exists(temppath)) { file.CopyTo(temppath, false); }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
