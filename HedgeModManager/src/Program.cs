4a5ba1f23c108637f754c0d16f2f891d7871c21b
thesupersonic16 <supersonic16bit@gmail.com>
2017-11-24 22:38:29 +1100

    Updated Version Number

--- HedgeModManager/src/Program.cs
+++ HedgeModManager/src/Program.cs
@@ -18,7 +18,7 @@ namespace HedgeModManager
         public static string GameName = "Unknown";
         public const string ProgramName = "Hedge Mod Manager";
         public const string ProgramNameShort = "HedgeModManager";
-        public const string VersionString = "6.1-009";
+        public const string VersionString = "6.1-010";
         public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
         public static bool Restart = false;
 
