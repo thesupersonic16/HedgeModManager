8af2cce01f83d4efb77a10b2f73dad21e064f0f4
sajidur rahaman <sajidur78@gmail.com>
2019-08-14 13:44:40 +0600

    stylish scrolly bois

--- HedgeModManager/App.xaml.cs
+++ HedgeModManager/App.xaml.cs
@@ -62,7 +62,7 @@ namespace HedgeModManager
             Steam.Init();
 #if DEBUG
             // Find a Steam Game
-            SteamGames = Steam.SearchForGames("Sonic Generations");
+            SteamGames = Steam.SearchForGames("Sonic Forces");
             var steamGame = SteamGames.FirstOrDefault();
             SelectSteamGame(steamGame);
             StartDirectory = steamGame.RootDirectory;
