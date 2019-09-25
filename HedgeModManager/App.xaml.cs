92e440d055bbc36684d769094f93a52fb933569d
thesupersonic16 <supersonic16bit@gmail.com>
2019-09-26 00:21:04 +1000

    Well so Intellisense didn't work

--- HedgeModManager/App.xaml.cs
+++ HedgeModManager/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
