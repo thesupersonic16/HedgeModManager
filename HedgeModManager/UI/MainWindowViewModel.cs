1218b2b190192368a509653f8e537d36f28b4b4e
thesupersonic16 <supersonic16bit@gmail.com>
2020-01-19 19:33:13 +1100

    Fixed Drag and Drop

--- HedgeModManager/UI/MainWindowViewModel.cs
+++ HedgeModManager/UI/MainWindowViewModel.cs
@@ -5,10 +5,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using GongSolutions.Wpf.DragDrop;
+using GongSolutions.Wpf.DragDrop.Utilities;
 
 namespace HedgeModManager.UI
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : INotifyPropertyChanged, IDropTarget
     {
         public CPKREDIRConfig CPKREDIR { get; set; }
         public ModsDB ModsDB { get; set; }
@@ -16,5 +19,31 @@ namespace HedgeModManager.UI
         public ObservableCollection<ModInfo> Mods { get; set; } = new ObservableCollection<ModInfo>();
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+            var dataObject = dropInfo.Data as IDataObject;
+            if (dataObject != null && dataObject.GetDataPresent(DataFormats.FileDrop))
+                dropInfo.Effects = DragDropEffects.Copy;
+            else
+                dropInfo.Effects = DragDropEffects.Move;
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            var dataObject = dropInfo.Data as DataObject;
+            if (dataObject != null && dataObject.ContainsFileDropList())
+            {
+                // Try Install mods from all files
+                foreach (string path in dataObject.GetFileDropList())
+                    ModsDB.InstallMod(path);
+            }
+            else
+            {
+                GongSolutions.Wpf.DragDrop.DragDrop.DefaultDropHandler.Drop(dropInfo);
+            }
+        }
     }
 }
