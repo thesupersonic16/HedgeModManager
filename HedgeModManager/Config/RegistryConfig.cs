2e33ad0771c9edb8c5ac581310b0ce2ad6e3823e
thesupersonic16 <supersonic16bit@gmail.com>
2021-04-22 00:20:53 +1000

    Added theme defaulting

--- HedgeModManager/Config/RegistryConfig.cs
+++ HedgeModManager/Config/RegistryConfig.cs
@@ -10,6 +10,7 @@ namespace HedgeModManager
     public static class RegistryConfig
     {
         private const string ConfigPath = @"SOFTWARE\HEDGEMM";
+        private const string PersonalizePath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
 
         public static string LastGameDirectory;
         public static string UILanguage;
@@ -31,10 +32,22 @@ namespace HedgeModManager
 
         public static void Load()
         {
+            // Personalisation for defaulting 
+            var personalizeKey = Registry.CurrentUser.OpenSubKey(PersonalizePath);
+            bool useLightMode = false;
+            if (personalizeKey != null)
+            {
+                var useLightThemeStr = personalizeKey.GetValue("AppsUseLightTheme", 0)?.ToString();
+                if (int.TryParse(useLightThemeStr, out int IuseLightTheme))
+                    useLightMode = IuseLightTheme != 0;
+
+                personalizeKey.Close();
+            }
+
             var key = Registry.CurrentUser.CreateSubKey(ConfigPath);
             LastGameDirectory = (string)key.GetValue("LastGame", string.Empty);
             UILanguage = (string)key.GetValue("UILanguage", HedgeApp.PCCulture);
-            UITheme = (string)key.GetValue("UITheme", "DarkTheme");
+            UITheme = (string)key.GetValue("UITheme", useLightMode ? "LightTheme" : "DarkTheme");
             key.Close();
         }
     }
