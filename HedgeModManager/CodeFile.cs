85b46db92e1dfb9490606eb528f6eb34e97f044c
HyperBE32 <34012267+HyperBE32@users.noreply.github.com>
2022-11-29 00:40:12 +0000

    [Codes] Move Author property to match metadata order (useless commit, don't look)

--- HedgeModManager/CodeFile.cs
+++ HedgeModManager/CodeFile.cs
@@ -114,9 +114,9 @@ namespace HedgeModManager
     {
         public string Name { get; set; }
 
-        public string Author { get; set; }
-
         public string Category { get; set; }
+
+        public string Author { get; set; }
 
         public string Description { get; set; }
 
