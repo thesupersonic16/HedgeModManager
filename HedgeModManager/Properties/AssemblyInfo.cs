31ebcc01255ee3a868c39c4f82d99807e5a85383
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-08-30 14:04:42 -0700

    Fix up executable details

--- HedgeModManager/Properties/AssemblyInfo.cs
+++ HedgeModManager/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HedgeModManager")]
-[assembly: AssemblyDescription("A mod manager for hedgehog engine games.")]
+[assembly: AssemblyTitle("A mod manager for Hedgehog Engine games.")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HedgeModManager")]
+[assembly: AssemblyProduct("Hedge Mod Manager")]
 [assembly: AssemblyCopyright("Copyright © NeverFinishAnything 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
