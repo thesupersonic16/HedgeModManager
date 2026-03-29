1ca1f01c697f6a0f2ea8cb9266fea9b5f7c81066
Sajid <sajidur78@gmail.com>
2026-03-29 18:00:27 +0600

    Mitigate TypeInitializationException via ILMerge

--- HedgeModManager/Properties/AssemblyInfo.cs
+++ HedgeModManager/Properties/AssemblyInfo.cs
@@ -4,6 +4,12 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+// Exclude bad IL merges
+// System Libraries
+// WPF Libraries, WPF references break with ILMerge
+// Mono.Cecil errors because I have no idea
+[assembly: ILMerge.ExcludeAssemblies(@"Mono.*|System.*|Microsoft.*|GongSolutions.WPF.DragDrop|PropertyTools.Wpf|HtmlRenderer\.WPF")]
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
